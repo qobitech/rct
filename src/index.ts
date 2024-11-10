7142bcdfaca23e7d97f6767ff180257b93a7cf84
Frank Qobi <qobitek@gmail.com>
2024-11-10 05:44:28 +0100

    feat: completed library setup

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,54 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./index.scss"
 
-import * as actions from "./components/action"
-import * as actionComponents from "./components/action-component"
-import * as breadCrumb from "./components/bread-crumb"
-import * as button from "./components/button"
-import * as checkbox from "./components/checkbox"
-import * as formBuilder from "./components/form-builder"
-import * as input from "./components/input"
-import * as inputSearch from "./components/input-search"
-import * as table from "./components/mini-table"
-import * as modal from "./components/new-modal"
-import * as upload from "./components/multiple-upload-document"
-import * as otp from "./components/otp"
-import * as pagination from "./components/pagination"
-import * as pills from "./components/pills"
-import * as rightSection from "./components/right-section"
-import * as select from "./components/select"
-import * as svg from "./components/svgs"
-import * as tabSection from "./components/tab-section"
-import * as tabToggler from "./components/tab-toggler"
-import * as textArea from "./components/text-area"
-import * as components from "./components/components"
-import * as timer from "./components/timer"
-import * as helper from "./components/helper"
-import * as hooks from "./components/hooks"
+// import * as actions from "./components/action"
+// import * as actionComponents from "./components/action-component"
+// import * as breadCrumb from "./components/bread-crumb"
+// import * as button from "./components/button"
+// import * as checkbox from "./components/checkbox"
+// import * as formBuilder from "./components/form-builder"
+export { TypeInput } from "./components/input"
+// import * as inputSearch from "./components/input-search"
+// import * as table from "./components/mini-table"
+// import * as modal from "./components/new-modal"
+// import * as upload from "./components/multiple-upload-document"
+// import * as otp from "./components/otp"
+// import * as pagination from "./components/pagination"
+// import * as pills from "./components/pills"
+// import * as rightSection from "./components/right-section"
+// import * as select from "./components/select"
+// import * as svg from "./components/svgs"
+// import * as tabSection from "./components/tab-section"
+// import * as tabToggler from "./components/tab-toggler"
+// import * as textArea from "./components/text-area"
+// import * as components from "./components/components"
+// import * as timer from "./components/timer"
+// import * as helper from "./components/helper"
+// import * as hooks from "./components/hooks"
 
-export {
-  actions,
-  actionComponents,
-  breadCrumb,
-  button,
-  checkbox,
-  formBuilder,
-  input,
-  inputSearch,
-  table,
-  components,
-  helper,
-  hooks,
-  modal,
-  upload,
-  otp,
-  pagination,
-  pills,
-  rightSection,
-  select,
-  svg,
-  tabSection,
-  tabToggler,
-  textArea,
-  timer,
-}
+// export {
+//   actions,
+//   actionComponents,
+//   breadCrumb,
+//   button,
+//   checkbox,
+//   formBuilder,
+//   input,
+//   inputSearch,
+//   table,
+//   components,
+//   helper,
+//   hooks,
+//   modal,
+//   upload,
+//   otp,
+//   pagination,
+//   pills,
+//   rightSection,
+//   select,
+//   svg,
+//   tabSection,
+//   tabToggler,
+//   textArea,
+//   timer,
+// }
